4232f1cf4a9371ca5318d94287506cbd0ade3175
Jefferson da Silva <je_jss@hotmail.com>
2020-11-19 03:49:28 -0300

    :star2: include img

---
+++ js/teste.js
@@ -0,0 +1,9 @@
+let number = 2;
+let valor1, valor2; 
+
+for (var contador1 = 1; (number >= contador1); contador1++) {
+    valor1 = contador1 * contador1;
+    valor2 = valor1 * contador1;
+
+    console.log(contador1, valor1, valor2)
+}
